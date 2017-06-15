da1f26072a5b78ed5ac898f5bde5a9c5ebe10a48
michael <mocegue8@calstatela.com>
2017-06-15 14:03:26 -0700

    First Push

---
+++ app/src/main/java/com/example/android/newsapp/NetworkUtils.java
@@ -0,0 +1,57 @@
+package com.example.android.newsapp;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class NetworkUtils {
+
+    public static final String TAG = "NetworkUtils";
+
+    public static final String NEWS_APP_API =
+            "https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey=26db1a84c65944c5b919ecb488383298";
+
+    public static final String PARA_QUERY = "q";
+
+    public static final String PARAM_SORT = "sort";
+
+
+    public static URL makeURL(String searchQuery, String sortBy){
+        Uri uri = Uri.parse(NEWS_APP_API).buildUpon()
+                .appendQueryParameter(PARA_QUERY, searchQuery)
+                .appendQueryParameter(PARAM_SORT, sortBy).build();
+
+        URL url = null;
+
+        try {
+            String urlString = uri.toString();
+            Log.d(TAG, "Url: " + urlString);
+            url = new URL(uri.toString());
+        }catch(MalformedURLException e){
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+            Scanner input = new Scanner(in);
+
+            input.useDelimiter("\\A");
+            return(input.hasNext())? input.next():null;
+
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
+}

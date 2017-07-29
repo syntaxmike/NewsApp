0ed4fc36b687bbd2ebf3e123982d427b4cae3228
michael <mocegue8@calstatela.com>
2017-07-28 23:50:50 -0700

    Changed DB name to avoid conflicts

--- app/src/main/java/com/example/android/newsapp/data/DBHelper.java
+++ app/src/main/java/com/example/android/newsapp/data/DBHelper.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 public class DBHelper extends SQLiteOpenHelper{
 
     private static final int DATABASE_VERSION = 1;
-    private static final String DATABASE_NAME = "news.db";
+    private static final String DATABASE_NAME = "THE_NEWS_DB_MO.db";
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);

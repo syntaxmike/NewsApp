f0ba7a4d6fcd8d7150f2d8f053bc845b39839e95
michael <mocegue8@calstatela.com>
2017-07-28 23:51:21 -0700

    Changed DB name to avoid conflicts

--- app/src/main/java/com/example/android/newsapp/utils/NetworkUtils.java
+++ app/src/main/java/com/example/android/newsapp/utils/NetworkUtils.java
@@ -39,7 +39,7 @@ public class NetworkUtils {
     * Hidden for security
     */
 
-    private static final String BASE_NEWS_KEY = "26db1a84c65944c5b919ecb488383298";
+    private static final String BASE_NEWS_KEY = "HIDDEN";
 
 
     public static URL makeURL(){
